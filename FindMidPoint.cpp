edcb9a7bdfb854c51d0df5af5513b6f749df9ea1
Deepak Navaria <dk17816@gmail.com>
2020-03-11 14:01:38 +0530

    MidpointofLL

--- FindMidPoint.cpp
+++ FindMidPoint.cpp
@@ -30,23 +30,30 @@ public:
 
 node* midpoint_linkedlist(node *head)
 {
-  node *t = head;
-  int c = 0;
-	while(t->next!=NULL){
-  	    c+=1;
-        t=t->next;
-    }
-  node *t1 = head;
-  c = c/2;
-    
-  while(t1){
- 	if(c==0){
-      return t1;
-    }
-    t1 = t1->next;
-    c--;
-  }
-    /*
+	if(head == NULL || head->next == NULL){
+		return head;
+	}
+
+
+  	node *t = head;
+  	int c = 0;
+  	while(t->next!=NULL){
+			c+=1;
+			t=t->next;
+		}
+	
+	  	node *t1 = head;
+	  	c = c/2;
+	 	
+	 while(t1){
+	 	if(c==0){
+	  	    return t1;
+	  	}
+		t1 = t1->next;
+	  	c--;
+  	}
+  
+  /* Second Method without finding length of linked list
   node *p = head;
   node *q = head;
   while(q->next->next!=NULL){
