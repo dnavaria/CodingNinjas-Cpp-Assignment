22b2d8fc22691b2dac051e7e0c6df065e99d8d60
Deepak Navaria <dk17816@gmail.com>
2020-03-14 17:41:58 +0530

    QueueUSingArray

---
+++ QueueUsingArray.cpp
@@ -0,0 +1,72 @@
+/*
+	Queue using Array
+*/
+
+#include <iostream> 
+using namespace std;
+
+
+template <typename T>
+class QueueUsingArray
+{
+	T *data;
+	int nextIndex;
+	int firstIndex;
+	int size;
+	int capactiy;
+
+public:
+	QueueUsingArray(int s){
+		data = new T[s];
+		nextIndex = 0;
+		firstIndex = -1;
+		size = 0;
+		capactiy = s;
+	}
+
+	int getSize(){
+		return size;
+	} 
+
+	bool isEmpty(){
+		return size==0;
+	}
+
+	void enqueue(T element){
+		if (size==capactiy){
+			cout<<"Queue Full"<<endl;
+			return;
+		}
+		data[nextIndex] = element;
+		nextIndex = (nextIndex+1)%capactiy;
+		if (firstIndex == -1)
+		{
+			firstIndex = 0;
+		}
+		size++;
+	}
+
+	T front(){
+		if (isEmpty()){
+			cout<<"Queue Empty !"<<endl;
+			return 0;
+		}
+		return data[firstIndex];
+	}
+
+	T dequeue(){
+		if (isEmpty()){
+			cout<<"Queue Empty !"<<endl;
+			return 0;
+		}
+
+		T ans = data[firstIndex];
+		firstIndex = (firstIndex+1)%capactiy;
+		size--;
+		if(size == 0){
+			firstIndex =-1;
+			nextIndex = 0;
+		}
+		return ans;
+	}
+};
