bec462628b0c3cdfc18bc40d5c33052bf618bb9f
Deepak Navaria <dk17816@gmail.com>
2020-03-24 22:41:35 +0530

    IsBst?

--- isBST.cpp
+++ isBST.cpp
@@ -20,10 +20,13 @@ false
 */
 
 
+// Approach - 1
+
 // T(N) = 2T(N/2) + K*N -> O(NLogN) - average case
 // T(N) = T(N-1) + K*N -> O(N^2) - worst case - skewed tree
-
-int maximum(BinaryTreeNode<int>* root){
+// Or O(N * H) where H is height
+/*
+	int maximum(BinaryTreeNode<int>* root){
 	if (root==NULL){
 		return INT_MIN;
 	}
@@ -50,4 +53,62 @@ bool isBST(BinaryTreeNode<int> *root){
 	bool Output = ((root->data > leftMax) && (root->data <= rightMin) && isBST(root->left) && isBST(root->right));
 
 	return Output;
+}
+*/
+
+
+
+// Approach - 2
+// T(n) = 2T(n/2) + K -> O(n)
+/*
+class IsBSTReturn{
+public:
+	bool isBST;
+	int minimum;
+	int maximum;
+};
+
+IsBSTReturn isBST(BinaryTreeNode<int> *root){
+	
+	if (root==NULL){
+		IsBSTReturn Output;
+		Output.isBST = true;
+		Output.minimum = INT_MAX;
+		Output.maximum = INT_MIN;
+		return Output;
+	}
+
+	IsBSTReturn left = isBST(root->left);
+	IsBSTReturn right = isBST(root->right);
+
+	int minimum = min(root->data, min(left.minimum, right.minimum));
+	int maximum = max(root->data, max(left.maximum, right.maximum));
+
+	bool isBSTFinal = (root->data > left.maximum) && (root->data <= right.minimum) && (left.isBST && right.isBST) ;
+
+	IsBSTReturn Output;
+	Output.minimum = minimum;
+	Output.maximum = maximum;
+	Output.isBST = isBSTFinal;
+
+	return Output;
+
+}
+
+*/
+
+// Approach - 3
+bool isBST(BinaryTreeNode<int> *root, int min=INT_MIN, int max=INT_MAX){
+	if (root==NULL){
+		return true;
+	}
+
+	if (root->data < min || root->data > max){
+		return false;
+	}
+
+	bool isLeftOk = isBST(root->left,min,root->data - 1);
+	bool isRightOk = isBST(root->right,root->data,max);
+
+	return isLeftOk && isRightOk;
 }
